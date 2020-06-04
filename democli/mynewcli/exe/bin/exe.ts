297fe81d01342d83297cf7d0c57f540bb7f4b6cb
Edwin <edwin.radtke@oblcc.com>
2020-06-04 20:11:10 +0200

    day4

--- democli/mynewcli/exe/bin/exe.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ExeStack } from '../lib/exe-stack';
-
-const app = new cdk.App();
-new ExeStack(app, 'ExeStack');
